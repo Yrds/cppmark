6fc35e5fe8993a1256139bf7f163011f80084cc1
Yuri dos Santos <yrds96@protonmail.com>
2022-09-27 13:39:45 -0300

    Scripts to development

---
+++ scripts.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+case $1 in
+ 'dev')
+   COMMAND="find src/ | entr meson test --verbose -C build ${@: 2}"
+    ;;
+  *)
+    echo "Please specify a command"
+    ;;
+esac
+
+
+if [ ${DRY_RUN=0} -eq 1 ]; then
+  echo $COMMAND
+else
+  bash -c "$COMMAND"
+fi
